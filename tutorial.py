335dac60b24f36dc018899d9568d18bcf7a4978f
Evan Jensen <jensen55@uw.edu>
2021-04-09 12:50:58 -0700

    tutorial_4

--- tutorial.py
+++ tutorial.py
@@ -1,18 +1,26 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/") 
 def home():
-    return "hello world, main page <h1> HI <h1>"
+    return render_template("index.html")
 
-@app.route("/<name>") 
-def user(name ):
-    return f"Hello {name}!"
-   
-@app.route("/admin") 
-def admin():
-    return redirect(url_for("home")) 
+# @app.route("/test")
+# def test():
+#     return render_template("new.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr =user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr} </h1>"
     
 if __name__ == "__main__":
-     app.run()
+     app.run(debug =True)
